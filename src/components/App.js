1762093fcbbd3b5c79f6f80e42f2469d04d89eaf
Priya <priya@Priyas-MacBook-Air.local>
2021-04-11 15:52:21 +0530

    add 20 products

--- src/components/App.js
+++ src/components/App.js
@@ -66,47 +66,47 @@ const App = () => {
 			},
 			{
 				id: 7,
-				image: "lehenga-1.jpeg",
-				title: "Classic Red Lehenga Choli",
-				oldPrice: 299.00,
-				newPrice: 260.00,
-				description: "Designer cotton bridal lehenga choli in red color. It comes with silk fabric...",
-				rating: '4.5',
-				colors: ["Red", "White", "Orange"]
+				image: "lehenga-7.jpeg",
+				title: "White and gold Lehenga Choli",
+				oldPrice: 220.00,
+				newPrice: 200.00,
+				description: "Designer net bridal lehenga choli in white color. Comes with golden border...",
+				rating: '3.5',
+				colors: ["White", "Red", "Orange"]
 			},
 			{
 				id: 8,
-				image: "lehenga-2.jpg",
-				title: "Red Embroidery Lehenga Choli",
-				oldPrice: 325.00,
-				newPrice: 299.00,
-				description: "It is paired with a matching color heavily embroidered cotton choli...",
-				rating: '4.5',
-				colors: ["Red", "White", "Orange"]
+				image: "lehenga-8.jpeg",
+				title: "Raw Silk Lehenga",
+				oldPrice: 275.00,
+				newPrice: 225.00,
+				description: "Embroidered Raw Silk Semi-Stitched Lehenga & Unstitched Choli...",
+				rating: '4.0',
+				colors: ["Orange", "White", "Blue"]
 			},
 			{
 				id: 9,
-				image: "lehenga-3.jpg",
-				title: "Multicolor Bridal Lehenga Choli",
+				image: "lehenga-9.jpeg",
+				title: "Red wedding lehenga",
 				oldPrice: 250.00,
 				newPrice: 225.00,
-				description: "Multicolor silk lehenga comes with a red silk heavily embroidered choli and ...",
-				rating: '5.0',
-				colors: ["Orange", "Green", "White"]
+				description: "Look super gorgeous in this red colored net bridal lehenga in satin...",
+				rating: '4.5',
+				colors: ["Red", "Green", "White"]
 			},
 			{
 				id: 10,
-				image: "lehenga-4.jpg",
-				title: "Embroidery Lehenga choli",
+				image: "lehenga-10.jpeg",
+				title: "Banglori Silk Lehenga choli",
 				oldPrice: 150.00,
 				newPrice: 130.00,
-				description: "Purple velvet lehenga choli with zari embroidery work all over. Comes with...",
-				rating: '4.0',
-				colors: ["Purple", "White", "Orange"]
+				description: "Black colour crepe fabric party wear lehenga choli comes with matching...",
+				rating: '3.0',
+				colors: ["Black", "Blue", "Orange"]
 			},
 			{
 				id: 11,
-				image: "lehenga-5.jpg",
+				image: "lehenga-11.jpeg",
 				title: "Maroon Lehenga Choli",
 				oldPrice: 250.00,
 				newPrice: 199.00,
@@ -116,7 +116,87 @@ const App = () => {
 			},
 			{
 				id: 12,
-				image: "lehenga-6.jpg",
+				image: "lehenga-12.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 13,
+				image: "lehenga-13.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 14,
+				image: "lehenga-14.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 15,
+				image: "lehenga-15.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 16,
+				image: "lehenga-16.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 17,
+				image: "lehenga-17.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 18,
+				image: "lehenga-18.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 19,
+				image: "lehenga-19.jpeg",
+				title: "Red Net Lehenga Choli",
+				oldPrice: 260.00,
+				newPrice: 229.00,
+				description: "Look beautiful in red color net bridal lehenga with thread embroidery work...",
+				rating: '4.0',
+				colors: ["Red", "Green", "Orange"]
+			},
+			{
+				id: 20,
+				image: "lehenga-20.jpeg",
 				title: "Red Net Lehenga Choli",
 				oldPrice: 260.00,
 				newPrice: 229.00,
